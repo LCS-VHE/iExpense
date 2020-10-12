d5279bdc7de865fce2752b0dda33bf3e9cc81d09
Vincent He <vincent0710@Outlook.com>
2020-10-12 17:48:33 -0400

    Using @ ObservedObject and @PUblish

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,16 +6,28 @@
 //
 
 import SwiftUI
-struct User{
-    var firstName:String
-    var lastName:String
-}
-struct ContentView: View {
-    @State private var user = User(firstName: "Bosco", lastName: "Choi")// Using Struct in swiftui class
+
+class User: ObservableObject{ // This is required when using Observed OBject
+    @Published var firstName = "Bosco" // Changing the view when the value  changes
+    @Published var lastName = "Choi" // Changing the view when the value  changes
     
+    init(fn: String, ln: String){
+        self.firstName = fn
+        self.lastName = ln
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject private var user = User(fn: "Bosco", ln: "Choi") // Data Sharing
+    ç
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Your Name is \(user.firstName), \(user.lastName)")
+            Section{
+                TextField("It Not What is your First Name", text:$user.firstName)
+                TextField("It Not What is your Last Name", text:$user.lastName)
+            }
+        }
     }
 }
 
